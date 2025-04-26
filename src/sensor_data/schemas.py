8a96bdbe04ddc5398488fc3d1409a02b0602e0f3
FoxedGuy <gradziuk.damian@o2.pl>
2025-04-26 18:16:06 +0200

    added sensor_data related schemas

---
+++ src/sensor_data/schemas.py
@@ -0,0 +1,41 @@
+from pydantic import BaseModel
+
+class Edge(BaseModel):
+    source: int
+    target: int
+    rssi: int
+
+class SensorNode(BaseModel):
+    id: int
+    longitude: float
+    latitude: float
+
+class Graph(BaseModel):
+    edges: list[Edge]
+    nodes: list[SensorNode]
+
+class SensorDataBase(BaseModel):
+    sensor_data_id: int
+
+class SensorDataCreate(BaseModel):
+    sensor_id: int
+    raw_packet: str
+    timestamp: str
+    noise: int
+    cpu_temp: int
+    free_heap: int
+    queue_fill: int
+    hop_ids: list[tuple[int, int]]
+
+class SensorDataWithGraph(BaseModel):
+    sensor_id: int
+    timestamp: str
+    raw_packet: str
+    noise: int
+    cpu_temp: int
+    free_heap: int
+    queue_fill: int
+    hop_ids: list[int]
+
+    class Config:
+        from_attributes = True
